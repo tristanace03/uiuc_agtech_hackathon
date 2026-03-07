990596c201955b01c94c7b36d13931e013a4c6a5
Hriday-S <hsz713x9@gmail.com>
2026-03-07 12:37:41 -0600

    justified height for tall corn, short corn, using µ_height, requirements.txt file

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import scipy
 # let short corn be 5 ft. tall, as a constant
 # let tall corn be 7 ft. tall, as a constant
 
-s = 5
-t = 7
+short_corn = 1.7 # https://www.researchgate.net/publication/223831105_Tollenaar_M_Lee_E_A_Yield_potential_yield_stability_and_stress_tolerance_in_maize_Field_Crop_Res_2002
+tall_corn = 2.81 # https://www.sciencedirect.com/science/article/pii/S0378429002001247
 
 # the force of wind
 f = lambda x: x
 
 # integrate force of wind by height (should be ds)
-s_total = scipy.integrate.quad(f, 0, s)[0]
-t_total = scipy.integrate.quad(f, 0, t)[0]
+# scipy.integrate.quad returns a tuple with (res, precision)
+s_total = scipy.integrate.quad(f, 0, short_corn)[0]
+t_total = scipy.integrate.quad(f, 0, tall_corn)[0]
 
 # arbitrary constants that depend on agregate of soil, identity of plant, etc
 root_sys_s = 1 
