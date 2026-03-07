4518c4f23fb66185e191e65e65cf3278a6e857d5
Hriday-S <hsz713x9@gmail.com>
2026-03-07 10:48:10 -0600

    physics modeling simple

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-import numpy as np
+import numpy as np  
+import scipy
 
-c = np.array([10,10,10,10,10,10,10])
+# let short corn be 5 ft. tall, as a constant
+# let tall corn be 7 ft. tall, as a constant
 
-x = np.array([0.1,0.3,0.3,0.7,0.6,0.5,0.1])
+s = 5
+t = 7
 
-ctx = c.T @ x
+# the force of wind
+f = lambda x: x
 
+# integrate force of wind by height (should be ds)
+s_total = scipy.integrate.quad(f, 0, s)[0]
+t_total = scipy.integrate.quad(f, 0, t)[0]
 
-print(ctx)
-print(ctx > 50)
+# arbitrary constants that depend on agregate of soil, identity of plant, etc
+root_sys_s = 1 
+root_sys_t = 1.5 
+
+s = s / root_sys_s
+t = t / root_sys_t
+
+print(s_total, t_total)
+
+# assume there is a function of wind, given break points, can estimate
